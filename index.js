c8f2fda6fc43f9b8b9ebe69690774f950c3d3225
xuan tran <xuandundkl@gmail.com>
2022-03-25 19:23:04 +0100

    change file structe and fix app function

--- index.js
+++ index.js
@@ -2,12 +2,55 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 1337;
 
+//exapmle task
+var task = ["do assignment", "drink juice"];
+var complete = ["finish"]
+
 //middleware
 app.use(express.json());
+app.use(express.urlencoded());
 
-//serving my static
-app.use(express.static("./public"));
+//set engile to make ejs file as html file
+app.set('view engine','ejs');
 
+//homepage
+app.get("/",(req,res)=>{
+    res.render("index",{task:task, complete:complete});
+});
+
+//post request
+app.post("/addtask", (req,res) =>{
+    //get value of newtask from input
+    let newTask = req.body.newtask;
+
+    //push new task to the array
+    task.push(newTask);
+
+    //go back to homepage
+    res.redirect('/');
+});
+
+//post request
+app.post("/deletetask", (req,res) =>{
+    //check if any value
+    let completeTask = req.body.check;
+
+    //if any task done, add to complete
+    if(typeof completeTask === "string"){
+        complete.push(completeTask);
+        //check if task is done, remove it from task list
+        task.splice(task.indexOf(completeTask),1);
+    }else if (typeof completeTask === "object"){
+        for (let i = 0; i < completeTask.length; i++){
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i],i));
+        }
+    }
+    res.redirect('/');
+
+});
+
+//http://localhost:1337
 app.listen(PORT, () =>{
     console.log(`port ${PORT} is running`)
 });
